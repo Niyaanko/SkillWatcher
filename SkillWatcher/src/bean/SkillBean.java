c9adac4e85193cbfd5163e5a179a1d5840ed7702
Niyaanko <grovvthpower@gmail.com>
2020-03-04 21:32:21 +0900

    SkillBean新規作成

---
+++ SkillWatcher/src/bean/SkillBean.java
@@ -0,0 +1,34 @@
+package bean;
+
+public class SkillBean {
+
+	private int id;
+	private String name;
+	private int size;
+	private int userId;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+}
