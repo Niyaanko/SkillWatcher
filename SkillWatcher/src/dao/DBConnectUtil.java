295513b88e13338ce4ba3487e55807af19e8a4ed
Niyaanko <grovvthpower@gmail.com>
2020-03-04 22:05:21 +0900

    DB接続クラス新規作成

---
+++ SkillWatcher/src/dao/DBConnectUtil.java
@@ -0,0 +1,32 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnectUtil {
+
+	//接続DBの宣言
+	private static final String DB = "SkillWatcher";
+	//接続USERの宣言
+	private static final String USER = "sa";
+	//接続PWの宣言
+	private static final String PW = "sa";
+
+	static Connection conn = null;
+
+	public DBConnectUtil() {
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			//DBへの接続情報
+
+			conn = DriverManager.getConnection("jdbc:sqlserver:"
+					+ "//localhost\\SQLEXPRESS;database=" + DB
+					+ "integratedSecurity=false;user=" + USER + ";password=sa" + PW);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
