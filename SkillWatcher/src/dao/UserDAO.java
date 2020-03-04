445e07dc91919dc267fd9df7ad5b8bce350e6bfd
Niyaanko <grovvthpower@gmail.com>
2020-03-04 23:38:36 +0900

    UserDAO getUserByMailAddress　メソッド新規作成

--- SkillWatcher/src/dao/UserDAO.java
+++ SkillWatcher/src/dao/UserDAO.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import bean.UserBean;
 
@@ -8,7 +9,7 @@ public class UserDAO extends DBConnectUtil{
 
 	private static final String TABLE = "";
 
-	/**
+	/**User新規登録メソッド
 	 * @param not {@code null}
 	 * @return INSERT文実行フラグ true/false
 	 */
@@ -30,6 +31,35 @@ public class UserDAO extends DBConnectUtil{
 		}catch(Exception e) {
 			e.printStackTrace();
 			return false;
+		}
+	}
+	/**User取得メソッド（メールアドレスから）
+	 * @param not {@code null}
+	 * @return UserBean User情報 取得できなかった場合 null
+	 */
+	public UserBean getUserByMailAddress(String mailAddress) {
+		try {
+			//User情報INSERT SQL
+			String sql = "SELECT user_id, mail_address, password, authority, user_name FROM" + TABLE
+						+ "WHERE mail_address = ?";
+
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1,mailAddress);
+			ResultSet rs = pStmt.executeQuery();
+			UserBean ub = null;
+			while(rs.next()) {
+				ub = new UserBean();
+				ub.setId(Integer.parseInt(rs.getString("user_id")));
+				ub.setMailAddress(rs.getString("mail_address"));
+				ub.setPassword(rs.getString("password"));
+				ub.setAuthority(Integer.parseInt(rs.getString("authority")));
+				ub.setName(rs.getString("user_name"));
+			}
+			return ub;
+
+		}catch(Exception e) {
+			e.printStackTrace();
+			return null;
 		}
 	}
 
